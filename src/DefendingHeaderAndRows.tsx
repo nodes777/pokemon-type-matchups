374385b9138749d87ed74b2d606f64d7d95ed73b
Taylor <taylor.nodell@gmail.com>
2020-07-14 15:20:38 +1000

    commit before using rows as state holders

--- src/DefendingHeaderAndRows.tsx
+++ src/DefendingHeaderAndRows.tsx
@@ -16,7 +16,11 @@ export const DefendingHeaderAndRows: React.FC<DefendingHeaderAndRowsProps> = ({}
 				const row = r + 1;
 				const ref = (RefManager[`row${row}col${1}`] = React.createRef());
 				return (
-					<tr role="row" className={styles.tableRowContainer} key={`Row${row}`}>
+					<tr
+						role="row"
+						className={styles.tableRowContainerStandard}
+						key={`Row${row}`}
+					>
 						<th
 							role="rowheader"
 							scope="row"
